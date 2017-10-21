79217c8c166731ddf64023bdcdf639049dfdd932
Aleksandar Trninkov <atrninkov1@studentmail.nait.ca>
2017-10-21 11:01:31 -0600

    made some changes to the sound slider and changed the secondary weapon fire sound

---
+++ Lab1PlayerMovement/Assets/BackgroundVolume.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class BackgroundVolume : MonoBehaviour {
+    [SerializeField]
+    AudioMixer mixer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float sliderValue;
+        mixer.GetFloat("Background Volume", out sliderValue);
+        GetComponent<Slider>().value = sliderValue;
+    }
+}
---
+++ Lab1PlayerMovement/Assets/MasterVolume.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MasterVolume : MonoBehaviour {
+    [SerializeField]
+    AudioMixer mixer;
+
+	// Update is called once per frame
+	void Update () {
+        float sliderValue;
+        mixer.GetFloat("Master Volume", out sliderValue);
+        GetComponent<Slider>().value = sliderValue;
+    }
+}
