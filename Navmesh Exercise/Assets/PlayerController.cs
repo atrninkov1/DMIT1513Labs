16597710fed9d87d5034ac760e2092c4791fa1c1
Aleksandar Trninkov <atrninkov1@studentmail.nait.ca>
2017-11-07 22:43:26 -0700

    commited something with the navmesh exercise

--- Navmesh Exercise/Assets/PlayerController.cs
+++ Navmesh Exercise/Assets/PlayerController.cs
@@ -7,7 +7,10 @@ public class PlayerController : MonoBehaviour
 {
 
     public List<NavMeshAgent> agents;
+    public GameObject bullet;
+
     RaycastHit hit;
+    float attackTimeStamp;
 
     // Use this for initialization
     void Start()
@@ -34,6 +37,32 @@ public class PlayerController : MonoBehaviour
                         agents[i].destination = hit.point;
                     }
                 }
+                if (hit.collider.gameObject.tag == "Enemy" && agents.Count > 0)
+                {
+                    for (int i = 0; i < agents.Count; i++)
+                    {
+                        agents[i].destination = hit.point;
+                        agents[i].stoppingDistance = 3;
+                        if (Vector3.Distance(hit.collider.transform.position, agents[i].transform.position) <= 4)
+                        {
+                            GameObject projectile = Instantiate(bullet);
+                            bullet.transform.position = agents[i].transform.position;
+                            bullet.transform.LookAt(hit.collider.gameObject.transform.position);
+                            projectile.GetComponent<Rigidbody>().AddForce(projectile.transform.forward * 100f);
+                        }
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < agents.Count; i++)
+        {
+            if (Vector3.Distance(hit.collider.transform.position, agents[i].transform.position) <= 3 && hit.collider.gameObject.tag == "Enemy" && Time.time > attackTimeStamp + 1)
+            {
+                GameObject projectile = Instantiate(bullet);
+                bullet.transform.position = agents[i].transform.position;
+                bullet.transform.LookAt(hit.collider.gameObject.transform.position);
+                projectile.GetComponent<Rigidbody>().AddForce(projectile.transform.forward * 100f);
+                attackTimeStamp = Time.time;
             }
         }
     }
